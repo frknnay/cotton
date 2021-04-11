3f97b9a7aa4447f269b5b2f853a807c3f48e792e
Furkan Ay <furkan-ay@outlook.com>
2021-04-12 02:10:49 +0300

    added title to card component

--- components/card/index.js
+++ components/card/index.js
@@ -1,7 +1,13 @@
 import styles from './style.module.scss';
 
-function Card({}) {
-  return <div className={styles['card']}></div>;
+function Card({ children }) {
+  return <div className={styles['card']}>{children}</div>;
 }
+
+function CardTitle({ children }) {
+  return <div className={styles['card-title']}>{children}</div>;
+}
+
+Card.Title = CardTitle;
 
 export { Card };
