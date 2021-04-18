8570f762532bd439bbd56c6c547ab30dbb0d39d9
Furkan Ay <furkan-ay@outlook.com>
2021-04-18 18:06:00 +0300

    added a sample board page

---
+++ pages/board/[id]/index.js
@@ -0,0 +1,33 @@
+import { Card } from '@components/card';
+import { List } from '@components/list';
+import { useRouter } from 'next/router';
+
+export default function Board() {
+  const router = useRouter();
+  const { id: boardId } = router.query;
+
+  return (
+    <div>
+      <List>
+        <List.Title>List Title</List.Title>
+        <List.CardContainer>
+          <Card>
+            <Card.Title>Decription title example.</Card.Title>
+          </Card>
+          <Card>
+            <Card.Title>Decription title example.</Card.Title>
+          </Card>
+          <Card>
+            <Card.Title>Decription title example.</Card.Title>
+          </Card>
+          <Card>
+            <Card.Title>Decription title example.</Card.Title>
+          </Card>
+          <Card>
+            <Card.Title>Decription title example.</Card.Title>
+          </Card>
+        </List.CardContainer>
+      </List>
+    </div>
+  );
+}
