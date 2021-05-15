35ba67e226194f92a3d457fc3bfa2a07ce8f1e56
Furkan Ay <furkan-ay@outlook.com>
2021-05-15 19:16:25 +0300

    added temporary link to board

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <Link href="/board/1">
+      <a>
+        <h2>Board 1</h2>
+      </a>
+    </Link>
+  );
 }
